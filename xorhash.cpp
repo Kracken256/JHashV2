04254b8bef616ff8d6a79ecac5206af632aa70d7
Wesley Jones <wesjones2004@gmail.com>
2022-12-29 18:02:54 -1000

    Update v2.

--- xorhash.cpp
+++ xorhash.cpp
@@ -23,41 +23,18 @@ int main(int argc, char *argv[])
     FILE *file = freopen(NULL, "rb", stdin);
     size_t bytesRead = 0;
     char buffer[32];
+    memset(buffer, 0, 32);
     if (file != NULL)
     {
         while ((bytesRead = fread(buffer, 1, 32, file)) > 0)
         {
-            if (bytesRead == 32)
+            uint8_t j = 0;
+            for (uint8_t i = 0; i < 32; i += 4, j++)
             {
-                int j = 0;
-                for (uint8_t i = 24; i < 32; i++, j++)
-                {
-                    state_4 ^= ((uint64_t)(buffer[i])) << (j * 8);
-                }
-            }
-            if (bytesRead >= 24)
-            {
-                int j = 0;
-                for (uint8_t i = 16; i < 24; i++, j++)
-                {
-                    state_3 ^= ((uint64_t)(buffer[i])) << (j * 8);
-                }
-            }
-            if (bytesRead >= 16)
-            {
-                int j = 0;
-                for (uint8_t i = 8; i < 16; i++, j++)
-                {
-                    state_2 ^= ((uint64_t)(buffer[i])) << (j * 8);
-                }
-            }
-            if (bytesRead >= 8)
-            {
-                int j = 0;
-                for (uint8_t i = 0; i < 8; i++, j++)
-                {
-                    state_1 ^= ((uint64_t)(buffer[i])) << (j * 8);
-                }
+                state_1 ^= ((uint64_t)(buffer[31 - i - 3])) << (j * 8);
+                state_2 ^= ((uint64_t)(buffer[31 - i - 2])) << (j * 8);
+                state_3 ^= ((uint64_t)(buffer[31 - i - 1])) << (j * 8);
+                state_4 ^= ((uint64_t)(buffer[31 - i - 0])) << (j * 8);
             }
         }
     }
