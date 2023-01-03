bba0eb6ea998c399003ec09e2fae836b64cf96b7
Wesley Jones <wesjones2004@gmail.com>
2023-01-02 14:56:50 -1000

    .

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-g++ jhashv2.cpp -o jhashv2 -O3 -Wall
-strip jhashv2
+g++ jhashv2.cpp -o bin/jhashv2 -O3 -Wall
+# x86_64-w64-mingw32-g++ jhashv2.cpp -o bin/jhashv2.exe -O3 -Wall -static-libgcc -static-libstdc++
